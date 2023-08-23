d2101c41a75a30eaae18ebf9d06f0cdb0989bb71
rrahmatn <rizkyrahmat292@gmail.com>
2023-08-23 23:01:40 +0700

    menyelesaikan navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect , useState } from "react";
+import { useLocation } from "react-router-dom";
+
+const Navbar = () => {
+  const [activeBot, setActivebot] = useState("home");
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setActivebot("home");
+    }
+    if (location.pathname === "/about") {
+      setActivebot("about");
+    }
+    if (location.pathname === "/project") {
+      setActivebot("project");
+    }
+    if (location.pathname === "/contact") {
+      setActivebot("contact");
+    }
+  }, []);
+
+  const activeNav = "bg-[#c5c6c7]/[0.5] text-[#1f2833]";
+
+
+  return (
+    <>
+      <div className="fixed mt-2 w-1/2 h-fit flex text-[#66fcf1] flex-row bg-[#1f2833]/[0.8] font-bold justify-between px-5 py-1 text-xl rounded-3xl">
+        <div 
+            id={activeBot === "home" ? "bg-[#c5c6c7]/[0.5] text-[#1f2833]" : ""}
+          onClick={() => {
+            setActivebot("home");
+          }}
+          className={`items-center  text-center cursor-pointer w-1/5 py-2 rounded-3xl hover:bg-[#c5c6c7]/[0.5]  ${activeBot === "home" ? activeNav : ""}`}
+        >
+          Home
+        </div>
+        <div
+          onClick={() => {
+            setActivebot("about");
+          }}
+          className={`items-center text-center cursor-pointer w-1/5 py-2 rounded-3xl hover:bg-[#c5c6c7]/[0.5]  ${activeBot === "about" ? activeNav : ""}`}
+        >
+          About
+        </div>
+        <div
+          onClick={() => {
+            setActivebot("project");
+          }}
+          className={`items-center text-center cursor-pointer w-1/5 py-2 rounded-3xl hover:bg-[#c5c6c7]/[0.5]  ${activeBot === "project" ? activeNav : ""}`}
+        >
+          Project
+        </div>
+        <div
+          onClick={() => {
+            setActivebot("contact");
+          }}
+          className={`items-center text-center cursor-pointer w-1/5 py-2 rounded-3xl hover:bg-[#c5c6c7]/[0.5]  ${activeBot === "contact" ? activeNav : ""}`}
+        >
+          Contact
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
--- src/page/dashboard/dashboard.jsx
+++ src/page/dashboard/dashboard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from "../../components/navbar/navbar";
 
 const Dashboard = () => {
     return(
         <>
-        <div className="bungkus-dashboard">
-            <h1>Hello World</h1>
+        <div className="w-full h-fit flex flex-cols justify-center">
+            <Navbar/>
         </div>
         </>
     )

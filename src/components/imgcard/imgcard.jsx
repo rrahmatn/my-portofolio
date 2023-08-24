c26e321e18c7bb107b0c649fed824b1b7a321b32
rrahmatn <rizkyrahmat292@gmail.com>
2023-08-24 22:34:57 +0700

    sadas

--- src/components/imgcard/imgcard.jsx
+++ src/components/imgcard/imgcard.jsx
@@ -5,7 +5,7 @@ const ImgCard = () => {
     <>
       <div className="w-[370px] h-[500px] rounded-t-[1000px] overflow-hidden shadow-xl">
         <div className="w-fit h-full overflow-hidden">
-          <img src="porto/formal.png" alt="" className="w-full h-fit" />
+          <img src="porto/formal(nonbg).png" alt="" className="w-full h-fit" />
         </div>
       </div>
     </>

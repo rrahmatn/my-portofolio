4363ced729dc1fca296657ded9e85c7563ac01bd
rrahmatn <rizkyrahmat292@gmail.com>
2023-08-24 22:30:28 +0700

    done

--- src/components/imgcard/imgcard.jsx
+++ src/components/imgcard/imgcard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ImgCard = ({ img }) => {
   return (
     <>
-      <div className="w-[370px] h-[500px] bg-indigo-500 rounded-t-[1000px] overflow-hidden shadow-xl">
+      <div className="w-[370px] h-[500px] rounded-t-[1000px] overflow-hidden shadow-xl">
         <div className="w-fit h-full overflow-hidden">
           <img src={img} alt="" className="w-full h-fit" />
         </div>
